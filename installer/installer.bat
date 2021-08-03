964b41597dc69a5e1de5ecbfa40bcdb8f76740aa
Bombo <88135556+BomboBombone@users.noreply.github.com>
2021-08-03 08:21:11 +0200

    Added installer and dependency programs
    Downloading the installer and pip with curl created some issues with the installer so they have been temporarily included.
    Need to make the installer delete the two extra files after the installation.
    Also need to add feature to create a shortcut for the main script

--- installer/installer.bat
+++ installer/installer.bat
@@ -1,1 +1,105 @@
+@echo off
+::sets the newline character
+set n=^&echo.
 
+cd %~dp0
+call :check_Permissions
+
+if %errorlevel% EQU 0 (
+call :check_python
+if %errorlevel% EQU 0 (
+	call :install_python
+	)
+if %errorlevel% EQU 0 (
+	call :install_pip
+	call :setup_pip
+	) else ( call :setup_pip )
+
+echo Tutto e' stato configurato correttamente, premi qualsiasi tasto per chiudere la console :D
+timeout /T 10 >nul
+exit 0
+)else (
+echo Premi qualsiasi tasto per uscire...
+timeout /T 10 >nul
+exit 1
+)
+
+::Checks if the console has been opened with admin privileges
+
+:check_Permissions
+    echo L'installazione richiede permessi di amministrazione. Controllando i permessi...%n%
+
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Successo: Permessi di amministratore confermati.%n%
+	EXIT /B 0
+    ) else (
+        echo Permessi d'amministratore non esistenti. Riavvia la console con permessi d'amministratore [ Start - cmd - tasto destro - esegui come amminsitratore ]%n%
+	EXIT /B 1
+    )
+
+::checks if python is already installed on the system
+
+:check_python
+echo Controllando se Python e' gia' installato%n%
+python -c "import os, sys; print(os.path.dirname(sys.executable))" >nul 2>&1
+if %errorlevel% NEQ 0 (
+echo Python e' gia' installato :O%n%
+EXIT /B 1
+) else (
+echo Python non e' installato :(%n%
+EXIT /B 0
+)
+
+
+::installs python
+
+:install_python
+echo Installando python, ci vorranno un paio di minuti al massimo ;)%n%
+python-installer.exe /quiet PrependPath=1 Include_test=0
+if %errorlevel% EQU 0 (
+echo Python installato con successo :D%n%
+exit /b 0)
+else (
+echo Ci sono stati problemi con l'installazione di python, riprova :(%n%
+exit /b 1
+)
+
+::check if pip is installed, and if it is, upgrade it to the latest version
+
+:check_pip
+echo Controllando se pip è già installato %n%
+pip help >nul 2>&1
+if %errorlevel% NEQ 0 (
+echo pip non e' installato.%n%
+exit /b 0
+) else (
+echo Aggiornando pip alla versione piu' recente...%n%
+call :upgrade_pip
+exit /b 1
+)
+
+::installs pip
+
+:install_pip
+echo Sto installando pip per te :D
+echo/
+python get-pip.py >nul 2>&1
+EXIT /B 0
+
+::upgrades pip to the latest version
+
+:upgrade_pip
+python -m pip install --upgrade pip >nul 2>&1
+echo/
+EXIT /B 0
+
+::setups pip for excel usage
+
+:setup_pip
+pip install xlwt >nul 2>&1
+pip install openpyxl >nul 2>&1
+pip install pysimplegui >nul 2>&1
+pip install xlrd >nul 2>&1
+pip install pandas >nul 2>&1
+EXIT /B 0
